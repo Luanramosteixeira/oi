548aef609128b4aa6610fc2a57eee6bcd4ad0268
guiespim18 <guilherme.monteiro.espim@gmail.com>
2023-10-09 10:56:48 -0300

    create player monster

---
+++ guilherme/dice.py
@@ -0,0 +1,6 @@
+import random
+
+def sort(): 
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    return dice1 + dice2
---
+++ guilherme/game.py
@@ -0,0 +1,9 @@
+import player
+import monster
+
+def main():
+    player.createPlayer()
+    monster.createMonster(2, 4)
+
+
+main()
